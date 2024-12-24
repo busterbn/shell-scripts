6ab5739d7a85373a3fb9423306a5ff67498c14e0
buster_bn <busternielsen99@gmail.com>
2024-12-24 18:14:08 +0100

    Added gbar setup

---
+++ gbar-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cargo install just
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+echo 'Installing pyenv.'
+curl https://pyenv.run | bash
+
+echo 'Adding pyenv to PATH and initializing it in .bashrc.'
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+echo "Run 'source ~/.bashrc' to apply the changes."
