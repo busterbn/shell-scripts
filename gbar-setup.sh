ff227d55e5ab6d014da20e1977259c0a2fbb5607
buster_bn <busternielsen99@gmail.com>
2024-12-24 19:11:52 +0100

    Fixed pyenv

--- gbar-setup.sh
+++ gbar-setup.sh
@@ -9,7 +9,10 @@ curl https://pyenv.run | bash
 
 echo 'Adding pyenv to PATH and initializing it in .bashrc.'
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+echo 'export PATH="$(pyenv root)/shims:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
 
 echo "Run 'source ~/.bashrc' to apply the changes."
